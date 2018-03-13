58e05545e8aa5287ca9a8a19abe72fec884ecf7f
Nat <nbenkul@gmail.com>
2018-03-13 12:31:06 +0700

    add image

--- index.js
+++ index.js
@@ -33,8 +33,18 @@ function handleEvent(event) {
 function handleMessageEvent(event) {
     var msg = {
         type: 'text',
-        text: 'สวัสดีครัช'
+        text: 'สวัสดีครับนี่เป็นระบบตอบกลับอัตโนมัติจาก CBA'
     };
+    var eventText = event.message.text.toLowerCase();
+
+    if (eventText === 'image') {
+        msg = {
+            'type': 'image',
+            'originalContentUrl': 'https://www.thesun.co.uk/wp-content/uploads/2017/03/fifa-17-2.jpg?strip=all&w=742&quality=100',
+            'previewImageUrl': 'https://images.performgroup.com/di/library/GOAL/a6/bb/fifa-18-ronaldo_lx3r88bpjpk91re36ukdgomrj.jpg?t=2027563652&w=620&h=430'
+        }
+    }
+
     return client.replyMessage(event.replyToken, msg);
 //Launch lintening server on port 80
 }
